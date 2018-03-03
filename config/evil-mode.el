04c33094250c7e2d6211af98ec0798c00847afed
Theo Andersen Carton <andersen.theo@gmail.com>
2018-03-03 14:47:36 +0100

    make emacs cursor green, and vim cursor keep the orange
    So that i know whenever Im suddently in emacs-mode :)

--- config/evil-mode.el
+++ config/evil-mode.el
@@ -2,4 +2,12 @@
   :init
   (progn
     (setq evil-default-cursor t)
-    (evil-mode 1)))
+    (add-hook 'evil-normal-state-entry-hook (lambda () (set-cursor-color "orange")))
+    (add-hook 'evil-insert-state-entry-hook (lambda () (set-cursor-color "orange")))
+    (add-hook 'evil-visual-state-entry-hook (lambda () (set-cursor-color "orange")))
+    (add-hook 'evil-emacs-state-entry-hook (lambda () (set-cursor-color "pink")))
+    (add-hook 'evil-operator-state-entry-hook (lambda () (set-cursor-color "orange")))
+    (add-hook 'evil-motion-state-entry-hook (lambda () (set-cursor-color "orange")))
+    (evil-mode 1)
+    )
+  )
